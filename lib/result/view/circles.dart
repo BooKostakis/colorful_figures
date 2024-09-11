8c2d043416d132f04c9bcbbc494a208acc957e53
Konstantin Bulanov <bookostakis@gmail.com>
2024-09-11 18:59:30 +0300

    mvp done! feat: user can see three tipes of figures an choose it in bottomNavigationBar

---
+++ lib/result/view/circles.dart
@@ -0,0 +1,70 @@
+import 'package:colorful_figures/colors_quantity/cubit/colors_quantity_cubit.dart';
+import 'package:colorful_figures/figures_quantity/cubit/figures_quantity_cubit.dart';
+import 'package:colorful_figures/result/view/rainbow_stack.dart';
+import 'package:colorful_figures/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class Circles extends StatelessWidget {
+  const Circles({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final figuresQuantityCubit = BlocProvider.of<FiguresQuantityCubit>(context);
+    final colorsQuantityCubit = BlocProvider.of<ColorsQuantityCubit>(context);
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter, // Начало градиента
+                end: Alignment.topCenter, // Конец градиента
+                colors: [
+                  Color.fromARGB(255, 0, 35, 80),
+                  Color.fromARGB(255, 0, 25, 50),
+                ],
+              ),
+            ),
+          ),
+          title: Text(
+            'Circles',
+            style: theme.textTheme.displayMedium,
+          ),
+        ),
+        body: Container(
+          color: theme.colorScheme.primary,
+          child: ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            itemCount: figuresQuantityCubit
+                .state.figuresQuantity, // Количество виджетов
+            itemBuilder: (context, index) {
+              return Wrap(
+                children: List.generate(
+                  1, // Количество виджетов в строке
+                  (index) => Center(
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 25,
+                        ),
+                        RainbowStack(
+                          maxWidth: 320,
+                          maxHeigth: 320,
+                          maxBorderRadius: 330,
+                          colorsQuantity:
+                              colorsQuantityCubit.state.colorsQuantity,
+                        ),
+                        const SizedBox(
+                          height: 25,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ));
+  }
+}
--- lib/result/view/result_page.dart
+++ lib/result/view/result_page.dart
@@ -1,5 +1,9 @@
 import 'package:colorful_figures/colors_quantity/cubit/colors_quantity_cubit.dart';
 import 'package:colorful_figures/figures_quantity/cubit/figures_quantity_cubit.dart';
+import 'package:colorful_figures/result/view/circles.dart';
+import 'package:colorful_figures/result/view/rainbow_stack.dart';
+import 'package:colorful_figures/result/view/squares.dart';
+import 'package:colorful_figures/result/view/stars.dart';
 import 'package:colorful_figures/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,45 +16,73 @@ class ResultPage extends StatefulWidget {
 }
 
 class _ResultPageState extends State<ResultPage> {
+  int _selectedTab = 1;
+
+  void onSelectedTab(int index) {
+    setState(() {
+      _selectedTab = index;
+    });
+  }
+
+  final List<Widget> _widgetOptions = <Widget>[
+    Stars(),
+    Circles(),
+    Squares(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    final figuresQuantityCubit = BlocProvider.of<FiguresQuantityCubit>(context);
-    final colorsQuantityCubit = BlocProvider.of<ColorsQuantityCubit>(context);
     return Scaffold(
+      floatingActionButton: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: BorderSide(width: 2, color: Colors.white)),
+              backgroundColor: Color.fromARGB(255, 100, 150, 0)),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(5, 8, 5, 5),
+            child: Text('Хочу еще!', style: theme.textTheme.bodyMedium),
+          )),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterFloat,
       backgroundColor: Theme.of(context).colorScheme.primary,
-      appBar: AppBar(
-        centerTitle: true,
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.bottomCenter, // Начало градиента
-              end: Alignment.topCenter, // Конец градиента
-              colors: [
-                Color.fromARGB(255, 0, 25, 50),
-                Color.fromARGB(255, 0, 35, 80),
-              ],
-            ),
+      bottomNavigationBar: Container(
+        height: 80,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter, // Начало градиента
+            end: Alignment.topCenter, // Конец градиента
+            colors: [
+              Color.fromARGB(255, 0, 25, 50),
+              Color.fromARGB(255, 0, 35, 80),
+            ],
           ),
         ),
-        title: Text(
-          'Colorful figures',
-          style: theme.textTheme.displayMedium,
-        ),
+        child: BottomNavigationBar(
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.white,
+            backgroundColor: Colors.transparent,
+            currentIndex: _selectedTab,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.star),
+                label: 'Звезды',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.circle),
+                label: 'Круги',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.square),
+                label: 'Квадраты',
+              ),
+            ],
+            onTap: onSelectedTab),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              'Количество цветов: ${colorsQuantityCubit.state.colorsQuantity}',
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-            Text(
-              'Количество фигур: ${figuresQuantityCubit.state.figuresQuantity}',
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-          ],
-        ),
-      ),
+      body: _widgetOptions[_selectedTab],
     );
   }
 }
